0c00ba974e1b654a75a38274ce508eecf0a68c76
Jean-Sébastien Pédron <jean-sebastien@yakaz.com>
2011-05-24 15:02:36 +0200

    Declare the record #unfinished_node

--- include/yaml_repr.hrl
+++ include/yaml_repr.hrl
@@ -1,25 +1,38 @@
+-ifndef(yaml_repr_hrl).
+-define(yaml_repr_hrl, true).
+
+-include("yaml_nodes.hrl").
+
 %% -------------------------------------------------------------------
 %% Options.
 %% -------------------------------------------------------------------
 
--type yaml_repr_option() :: {node_mods, [atom()]}
-                          | {simple_structs, boolean()}.
+-type yaml_repr_option() :: {simple_structs, boolean()}.
 
 %% -------------------------------------------------------------------
 %% Representation state.
 %% -------------------------------------------------------------------
+
+-record(unfinished_node, {
+    module = ?MODULE,
+    path,
+    pres,
+    priv
+  }).
 
 -record(yaml_repr, {
     options              = []         :: [yaml_repr_option()],
     simple_structs       = true       :: boolean(),
     mods                 = []         :: [atom()],
     tags                 = []         :: [{tag_uri(), atom()}],
-    docs                 = []         :: [yaml_document() |
-                                          yaml_simple_document()],
+    docs                 = []         :: [yaml_doc() |
+                                          yaml_simple_doc()],
     docs_count           = 0          :: non_neg_integer(),
-    current_doc          = undefined  :: [yaml_partial_document() |
+    current_doc          = undefined  :: [yaml_partial_doc() |
                                           yaml_partial_node() |
                                           undefined],
     current_node_is_leaf = false      :: boolean(),
     anchors              = dict:new() :: dict()
   }).
+
+-endif.
