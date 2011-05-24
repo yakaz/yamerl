3f56c3e68a1f894827e301e35bc13d8c75ffb536
Jean-Sébastien Pédron <jean-sebastien@yakaz.com>
2011-05-24 14:51:32 +0200

    Add yaml_app and yaml_sup modules
    The yaml_app module is an application behaviour used to check the
    configuration. The yaml_sup module is a supervisor behaviour only
    included because an application must start a supervision tree.

---
+++ src/yaml_sup.erl
@@ -0,0 +1,35 @@
+-module(yaml_sup).
+
+-behaviour(supervisor).
+
+%% Initialization.
+-export([
+    start_link/0
+  ]).     
+
+%% supervisor(3erl) callbacks.
+-export([   
+    init/1      
+  ]).           
+
+%% -------------------------------------------------------------------
+%% Initialization.
+%% -------------------------------------------------------------------
+
+-spec start_link() ->
+    {ok, pid()} |
+    ignore      |
+    {error, {already_started, pid()} | shutdown | term()}.
+
+start_link() ->
+    supervisor:start_link({local, ?MODULE}, ?MODULE, []).
+
+%% -------------------------------------------------------------------
+%% supervisor(3erl) callbacks.
+%% -------------------------------------------------------------------
+
+init(_) ->
+    {ok, {
+        {one_for_one, 10, 1}, [
+        ]
+      }}.
