b2178057b8be8d0414f5d32b20821d29ff6169a0
Jon Gretar <jongretar@jongretar.com>
2014-10-20 17:28:57 +0000

    Elixir/hex.pm support

---
+++ package.exs
@@ -0,0 +1,30 @@
+defmodule Poolboy.Mixfile do
+  use Mix.Project
+
+  def project do
+    [
+      app: :yamerl,
+      version: "0.3.1-1",
+      description: description,
+      package: package,
+      deps: []
+    ]
+  end
+
+  defp description do
+    """
+    YAML 1.2 parser in pure Erlang
+    """
+  end
+
+  defp package do
+    [
+      files: ~w(src doc include testsuite rebar.config README.md AUTHORS COPYING),
+      contributors: ["Yakaz", "Jean-Sébastien Pédron"],
+      licenses: ["BSD 2-Clause"],
+      links: %{
+        "GitHub" => "https://github.com/yakaz/yamerl",
+        "Doc" => "https://github.com/yakaz/yamerl/tree/master/doc"
+      }]
+   end
+end
