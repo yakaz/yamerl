b9ec051b127b5714c23fe566fe67de5fb96aec3e
Jean-Sébastien Pédron <jean-sebastien.pedron@dumbbell.fr>
2016-11-13 16:54:10 +0100

    test/construction/str.erl: Add tests for JSON strings

--- test/construction/str.erl
+++ test/construction/str.erl
@@ -108,3 +108,115 @@ binary_utf32be_simple_detailed_test_() ->
         )
       ]
     }.
+
+list_schema_json_simple_test_() ->
+    {setup,
+      fun setup/0,
+      [
+        ?_assertMatch(
+          [
+            [$2, $\s, 8364] % "2 €"
+          ],
+          yamerl_constr:file(?FILENAME, [
+              {schema, json},
+              {detailed_constr, false}
+            ])
+        )
+      ]
+    }.
+
+list_schema_json_detailed_test_() ->
+    {setup,
+      fun setup/0,
+      [
+        ?_assertMatch(
+          [
+            {yamerl_doc,
+              {yamerl_str,yamerl_node_str_json,"tag:yaml.org,2002:str",
+                [{line,1},{column,1}],
+                [$2, $\s, 8364]} % "2 €"
+            }
+          ],
+          yamerl_constr:file(?FILENAME, [
+              {schema, json},
+              {detailed_constr, true}
+            ])
+        )
+      ]
+    }.
+
+binary_schema_json_simple_test_() ->
+    {setup,
+      fun setup/0,
+      [
+        ?_assertMatch(
+          [
+            <<50,32,226,130,172>> % "2 €"
+          ],
+          yamerl_constr:file(?FILENAME, [
+              {schema, json},
+              {detailed_constr, false},
+              str_node_as_binary
+            ])
+        )
+      ]
+    }.
+
+binary_schema_json_detailed_test_() ->
+    {setup,
+      fun setup/0,
+      [
+        ?_assertMatch(
+          [
+            {yamerl_doc,
+              {yamerl_str,yamerl_node_str_json,"tag:yaml.org,2002:str",
+                [{line,1},{column,1}],
+                <<50,32,226,130,172>>} % "2 €"
+            }
+          ],
+          yamerl_constr:file(?FILENAME, [
+              {schema, json},
+              {detailed_constr, true},
+              str_node_as_binary
+            ])
+        )
+      ]
+    }.
+
+binary_utf32be_schema_json_simple_test_() ->
+    {setup,
+      fun setup/0,
+      [
+        ?_assertMatch(
+          [
+            <<0,0,0,50, 0,0,0,32, 0,0,32,172>> % "2 €"
+          ],
+          yamerl_constr:file(?FILENAME, [
+              {schema, json},
+              {detailed_constr, false},
+              {str_node_as_binary, {utf32, big}}
+            ])
+        )
+      ]
+    }.
+
+binary_utf32be_schema_json_detailed_test_() ->
+    {setup,
+      fun setup/0,
+      [
+        ?_assertMatch(
+          [
+            {yamerl_doc,
+              {yamerl_str,yamerl_node_str_json,"tag:yaml.org,2002:str",
+                [{line,1},{column,1}],
+                <<0,0,0,50, 0,0,0,32, 0,0,32,172>>} % "2 €"
+            }
+          ],
+          yamerl_constr:file(?FILENAME, [
+              {schema, json},
+              {detailed_constr, true},
+              {str_node_as_binary, {utf32, big}}
+            ])
+        )
+      ]
+    }.
